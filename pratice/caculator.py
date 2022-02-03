81a37a5def28d2c0248a2935fa3bbffffa897f9b
sampath <sampathnimmala2@gmail.com>
2022-02-03 18:40:43 +0530

    data types

---
+++ pratice/caculator.py
@@ -0,0 +1,28 @@
+num1 = 10
+num2 = 8
+def add(value1,value2):
+
+    print("add",value1+value2)
+
+add(num1,num2)
+
+def sub(value1,value2):
+
+    print("sub",value1-value2)
+
+sub(num1,num2)
+
+def mul(value1,value2):
+
+    print("mul",value1*value2)
+
+mul(num1,num2)
+
+def div(value1,value2):
+
+    print("div",value1/value2)
+
+div(num1,num2)
+
+
+
---
+++ pratice/data types.py
@@ -0,0 +1,32 @@
+name = "samapth"
+number = 8
+flag = True
+flag1 = False
+
+float = 20.5
+complex = 1j
+list = ["apple", "banana", "cherry",4]
+tuple = ("apple", "banana", "cherry",1)
+range = range(6)
+dict = {"name" : "John", "age" : 36}
+set	 = {"apple", "banana", "cherry"}
+xfrozenset = frozenset({"apple", "banana", "cherry"})
+bytes = b"Hello"
+bytearray = bytearray(5)
+#memoryview = memoryview(bytes(6))
+
+print(flag)
+print(flag1)
+print(name)
+print(number)
+print(float)
+print(complex)
+print(list)
+print(tuple)
+print(range)
+print(dict)
+print(set)
+print(xfrozenset)
+print(bytes)
+print(bytearray)
+print(memoryview)
